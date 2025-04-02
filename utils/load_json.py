bc0b829e3ef72d15d562acc8775851b785077026
brandon.lee <brandon.lee@gamehours.com>
2025-04-02 17:14:00 +0800

    generate .exe

--- utils/load_json.py
+++ utils/load_json.py
@@ -1,8 +1,13 @@
 """讀取 json 文件"""
 import os
 import json
+import sys
 
-CONFIG_PATH = os.path.join(os.getcwd(), "api_config.json")
+# CONFIG_PATH = os.path.join(os.getcwd(), "api_config.json")
+# 自動取得打包 or 本地的路徑
+base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+CONFIG_PATH = os.path.join(base_path, '..', "api_config.json")
+
 
 def get_config(key):
     """ 讀取 JSON 設定檔的 key 的鍵值 """
